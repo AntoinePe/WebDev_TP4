3be5df837c2127d624a9b573d501459b060ffe33
Antoine Pelletier <antoine.pelleiter@eurecom.fr>
2018-11-06 18:34:33 -0500

    Adding confirmation and contact pug.

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,49 @@
 const express = require("express");
 const router = express.Router();
 
+const nbItem=5;
+const menuActif="";
+const firstNameOrder=""
+const nameOrder=""
+
 router.get("/", (req, res) => {
+	menuActif="Acceuil"
+	res.
   	res.render("index", { 
   							title: "Accueil", 
+  							count: nbItem,
   							message: "Le site n°1 pour les achats en ligne !",
-  							description: "Découvrez nos différents produits au meilleur prix."
+  							description: "Découvrez nos différents produits au meilleur prix.",
   						})
 });
 
-router.get("/acceuil", (req,res) =>{
+router.get("/acceuil", (req,res) => {
+	menuActif="Acceuil"
 	res.render("index", { 
 							title:"Acceuil", 
+							count: nbItem,
 							message: "Le site n°1 pour les achats en ligne !",
-							description: "Découvrez nos différents produits au meilleur prix."
+							description: "Découvrez nos différents produits au meilleur prix.",
+							selected: "Acceuil"
 						})
 });
 
+router.get("/confirmation", (req,res)=>{
+	menuActif=""
+	res.render('confirmation',{
+								confirmation:"Votre commande est confirmée "+firstNameOrder+" "+nameOrder+"!",
+								numeroConf: "Votre numéro de confirmation est le <strong>00001</strong>."
+	})
+});
+
+
+router.get("/contact", (req,res)=>{
+	menuActif="Contact"
+	res.render('contact',{
+							count:nbItem
+	})
+
+});
 /*
 router.get("/produits", (res,res)=>{
 	res.render("produits", {
@@ -26,13 +53,10 @@ router.get("/produits", (res,res)=>{
 
 router.get("/produit/:id", (res,res)=>{});
 
-router.get("/contact", (res,res)=>{});
-
 router.get("/panier", (res,res)=>{});
 
 router.get("/commande", (res,res)=>{});
 
-router.get("/confirmation", (res,res)=>{});
 */
 
 module.exports = router;

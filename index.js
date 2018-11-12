bed478dbd73c6e1c275abff625ac9745c9c974d4
Remy64 <35994712+Remy64@users.noreply.github.com>
2018-11-11 23:18:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+
+var nbItem=5;
+var menuActif="";
+var firstNameOrder=""
+var nameOrder=""
+
+router.get("/", (req, res) => {
+	menuActif="Accueil";
+  	res.render("index", { 
+  							title: "Accueil", 
+  							count: nbItem,
+  							message: "Le site n°1 pour les achats en ligne !",
+  							description: "Découvrez nos différents produits au meilleur prix.",
+  						})
+});
+
+router.get("/accueil", (req,res) => {
+	menuActif="Accueil";
+	res.render("index", { 
+							title:"Accueil", 
+							count: nbItem,
+							message: "Le site n°1 pour les achats en ligne !",
+							description: "Découvrez nos différents produits au meilleur prix.",
+							selected: "Accueil"
+						})
+});
+
+router.get("/confirmation", (req,res)=>{
+	menuActif=""
+	res.render('confirmation',{
+								title:"Confirmation",
+								confirmation:"Votre commande est confirmée "+firstNameOrder+" "+nameOrder+"!",
+								numeroConf: "Votre numéro de confirmation est le <strong>00001</strong>."
+	})
+});
+
+
+router.get("/contact", (req,res)=>{
+	menuActif="Contact"
+	res.render('contact',{
+							title:"Contact",
+							count:nbItem
+	})
+
+});
+
+router.get("/produits", (req,res)=>{
+	menuActif="Produits"
+	res.render("produits", {
+								title: "Produits",
+								titre: "Produits",
+								count: nbItem
+	})
+});
+router.get("/panier", (req,res)=>{
+	menuActif="Panier"
+	res.render("panier", {
+		title: "Panier",
+		titre: "Panier"
+
+
+	})
+});
+router.get("/commande", (req,res)=>{
+	menuActif="Commande"
+	res.render("commande", {
+		title: "Commande",
+		titre: "Commande"
+
+	})
+});
+/*router.get("/produit/:id", (req,res)=>{});
+
+
+
+
+
+*/
+
+module.exports = router;

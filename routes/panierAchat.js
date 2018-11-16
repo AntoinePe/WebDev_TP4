e7b9b558f6b7aa93840ba6942180bab1b172a883
Remy64 <35994712+Remy64@users.noreply.github.com>
2018-11-15 23:36:19 -0500

    Add files via upload

--- routes/panierAchat.js
+++ routes/panierAchat.js
@@ -1,3 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const rest=require("restler");
+router.get("/api/shopping-cart", (req, res) => {
+	let panier = req.session.shopping-cart;
+	if (panier == undefined) {
+		panier = [];	
+	}
+	res.status(200).send(panier);
+
+	})
+
+router.get("/api/shopping-cart/:productId", (req, res) => {
+
+let panier = req.session.shopping-cart;
+let produitDemande;
+let trouve = false;
+if (panier == undefined) {
+
+	res.sendStatus(404) ;
+}
+panier.forEach(function(produit) {
+
+	if (produit.id == productId) {
+		produitDemande = produit ;
+		trouve = true;
+
+	}
+})
+if (trouve) {
+
+	res.status(200).send(produitDemande);
+}
+else {
+	res.sendStatus(404) ;
+}
+})
+
+router.post("/api/shopping-cart", (req, res) => {
+
+	
+
+
+})
+
+router.delete("/api/shopping-cart", (req, res) => {
+req.session.shopping-cart = [];
+res.sendStatus(204);
+
+})
